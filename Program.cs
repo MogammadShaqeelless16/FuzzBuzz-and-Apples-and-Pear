068c7094ee6383a8b92ed5e56f4e14a0eae82cb9
MogammadShaqeelless16 <54390756+MogammadShaqeelless16@users.noreply.github.com>
2019-08-22 08:52:38 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("ApplesPears");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Apples");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Pear");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
+
+
